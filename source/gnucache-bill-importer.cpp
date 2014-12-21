0c3f76354c9442f7b362d9b72a5825c1a152c870
nyquistdev <nyquistdev@outlook.com>
2014-12-21 20:38:30 +0100

    Add ImageLoaderException

--- source/gnucache-bill-importer.cpp
+++ source/gnucache-bill-importer.cpp
@@ -1,9 +1,22 @@
 #include "gnucache-bill-importer.h"
 
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <functional>
 using namespace std;
+
+ImageLoaderException::ImageLoaderException(std::string fileName, unsigned int lineNumber,
+    std::string message)
+  : m_fileName(fileName), m_lineNumber(lineNumber), m_message(message) 
+{ 
+ //TODO logging
+}
+
+const char* ImageLoaderException::what() const noexcept
+{
+ return m_message.c_str(); 
+}
 
 int main()
 {
--- source/gnucache-bill-importer.h
+++ source/gnucache-bill-importer.h
@@ -4,7 +4,7 @@
 #if defined(_MSC_VER)
     #define EXPORT __declspec(dllexport)
     #define IMPORT __declspec(dllimport)
-#elif defined(_GCC)
+#elif defined(__GNUG__)
     #define EXPORT __attribute__((visibility("default")))
     #define IMPORT
 #elif defined(__llvm__)
@@ -13,5 +13,20 @@
 #else
     #error Unknown dynamic link import/export semantics.
 #endif
+
+#include <exception>
+#include <string>
+
+class ImageLoaderException : public std::exception 
+{
+ private:
+   std::string m_fileName;
+   unsigned int m_lineNumber;
+   std::string m_message;
+
+ public:
+   ImageLoaderException(std::string, unsigned int, std::string);
+   const char* what() const noexcept;
+};
 
 #endif /* end of include guard: GNUCACHE_BILL_IMPORTER_H_EUZYAZPN */
